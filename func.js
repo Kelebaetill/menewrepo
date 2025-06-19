9ac30cced532f5640fcde3c0ed56992967b48429
Tem <kelebaetill@gmail.com>
2025-06-19 17:05:13 +0200

    Fixed error

--- func.js
+++ func.js
@@ -1,5 +1,5 @@
 function greetUser(name) {
-  if (name = "Admin") { // ❌ Ошибка: используется оператор присваивания (=) вместо оператора сравнения (===)
+  if (name === "Admin") { // ❌ Ошибка: используется оператор присваивания (=) вместо оператора сравнения (===)
     console.log("Welcome, administrator!");
   } else {
     console.log("Hello, " + name + "!");
